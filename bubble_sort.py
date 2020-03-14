8b8f89f3b6ac251366e9b5a02d961e7eee293851
yamato mitsui <yamato.mitsui.orleans@gmail.com>
2020-03-15 01:40:09 +0900

    modify a bubble sort

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,12 +1,16 @@
 def bubble_sort(A, N):
     flag = 1
+    cnt = 0
     while flag:
         flag = 0
         for j in range(N - 1, 0, -1):
             if A[j] < A[j - 1]:
                 A[j], A[j -1] = A[j - 1], A[j]
                 flag = 1
-                print(A)
+                cnt += 1
+    print(' '.join(list(map(str, A))))
+    print(cnt)
+                
 
 N = int(input())
 A = list(map(int, input().split()))
