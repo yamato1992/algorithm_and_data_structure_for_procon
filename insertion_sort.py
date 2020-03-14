a60ea864c9efa44449fca4b146f653a406bc8148
yamato mitsui <yamato.mitsui.orleans@gmail.com>
2020-03-15 00:57:35 +0900

    modify an insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,11 +2,12 @@ def insertion_sort(A, N):
     for i in range(1, N):
         v = A[i]
         j = i - 1
-        print(A)
+        print(' '.join(list(map(str, A))))
         while j >= 0 and A[j] > v:
             A[j + 1] = A[j]
             j -= 1
         A[j + 1] = v
+    print(' '.join(list(map(str, A))))
 
 N = int(input())
 A = list(map(int, input().split()))
