365c2fcae246e91c3319fcaba9a258ca0a59c507
yamato mitsui <yamato.mitsui.orleans@gmail.com>
2020-03-15 02:38:52 +0900

    modify a selection sort

--- selection_sort.py
+++ selection_sort.py
@@ -1,11 +1,15 @@
 def selection_sort(A, N):
+    cnt = 0
     for i in range(N):
         min_index = i
         for j in range(i, N):
             if A[j] < A[min_index]:
                 min_index = j
-        A[i], A[min_index] = A[min_index], A[i]
-        print(A)
+        if i != min_index:
+            A[i], A[min_index] = A[min_index], A[i]
+            cnt += 1
+    print(' '.join(list(map(str, A))))
+    print(cnt)
 
 
 N = int(input())
